400df41aa86e5ffa1fef79f8ae4603f28889353f
SebWhipps-Br <123549554+SebWhipps-Br@users.noreply.github.com>
2023-11-23 10:54:49 +0000

    cleanup

--- stubs/stubs.go
+++ stubs/stubs.go
@@ -4,18 +4,18 @@ import "uk.ac.bris.cs/gameoflife/util"
 
 // capital letters for exported types
 // game of life operations and processed turns
+
 var Handler = "GameOfLifeOperations.UpdateWorld"
 var GetAliveCount = "GameOfLifeOperations.GetAliveCount"
 var GetCurrentWorld = "GameOfLifeOperations.GetCurrentWorld"
 var HaltServer = "GameOfLifeOperations.HaltServer"
 
-// final world returned
 type Response struct {
 	NextWorld      []util.BitArray
 	CompletedTurns int
 }
 
-// contains num of turns, 2d slice (initial state), size of image
+// Request contains num of turns, 2d slice (initial state), size of image
 type Request struct {
 	Turns       int
 	ImageWidth  int
