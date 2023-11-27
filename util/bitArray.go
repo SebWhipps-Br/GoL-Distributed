e80f08b91362d6155a75b56f3299b011125d9f49
SebWhipps-Br <123549554+SebWhipps-Br@users.noreply.github.com>
2023-11-27 10:09:10 +0000

    clean up bitArray.go

--- util/bitArray.go
+++ util/bitArray.go
@@ -7,38 +7,38 @@ func NewBitArray(n int) BitArray {
 }
 
 func (b BitArray) SetBit(index int, value bool) {
-	pos := index / 8
-	j := uint(index % 8)
+	position := index / 8
+	i := uint(index % 8)
 	if value {
-		b[pos] |= (uint8(1) << j)
+		b[position] |= uint8(1) << i
 	} else {
-		b[pos] &= ^(uint8(1) << j)
+		b[position] &= ^(uint8(1) << i)
 	}
 }
 
 func (b BitArray) SetBitFromUint8(index int, value uint8) {
-	pos := index / 8
-	j := uint(index % 8)
-	if value == 255 {
-		b[pos] |= (uint8(1) << j)
+	position := index / 8
+	i := uint(index % 8)
+	if value == 0 {
+		b[position] &= ^(uint8(1) << i)
 	} else {
-		b[pos] &= ^(uint8(1) << j)
+		b[position] |= uint8(1) << i
 	}
 }
 
 func (b BitArray) GetBit(index int) bool {
 	// Calculate the position in the array where the bit is stored.
-	pos := index / 8
+	position := index / 8
 
 	// Calculate the position of the bit within the uint8 at the calculated position.
-	bitPos := uint(index % 8)
+	bitPosition := uint(index % 8)
 
 	// Create a mask by shifting 1 to the left by the bit position.
-	mask := uint8(1) << bitPos
+	mask := uint8(1) << bitPosition
 
 	// Use bitwise AND to get the value of the bit at the bit position in the uint8.
 	// If the bit is set, the result will be non-zero.
-	bitIsSet := b[pos] & mask
+	bitIsSet := b[position] & mask
 
 	// Return true if the bit is set, false otherwise.
 	return bitIsSet != 0
@@ -46,17 +46,17 @@ func (b BitArray) GetBit(index int) bool {
 
 func (b BitArray) GetBitToUint8(index int) uint8 {
 	// Calculate the position in the array where the bit is stored.
-	pos := index / 8
+	position := index / 8
 
 	// Calculate the position of the bit within the uint8 at the calculated position.
-	bitPos := uint(index % 8)
+	bitPosition := uint(index % 8)
 
 	// Create a mask by shifting 1 to the left by the bit position.
-	mask := uint8(1) << bitPos
+	mask := uint8(1) << bitPosition
 
 	// Use bitwise AND to get the value of the bit at the bit position in the uint8.
 	// If the bit is set, the result will be non-zero.
-	bitIsSet := b[pos] & mask
+	bitIsSet := b[position] & mask
 
 	// Return 1 if the bit is set, 0 otherwise.
 	if bitIsSet != 0 {
